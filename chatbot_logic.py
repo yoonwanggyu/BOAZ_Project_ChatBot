02ecae6e1054bb75e81214f6a4ad732e11d7f9c2
김동환 <forwarder1121@naver.com>
2024-08-28 01:01:03 +0900

    EDIT : LLM model change

--- chatbot_logic.py
+++ chatbot_logic.py
@@ -2,17 +2,21 @@ import os
 from dotenv import load_dotenv
 from pinecone import Pinecone
 from langchain_openai import ChatOpenAI
-from langchain.chains import ConversationalRetrievalChain
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
 from langchain.chains import create_history_aware_retriever
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import CrossEncoderReranker
+from langchain_community.cross_encoders import HuggingFaceCrossEncoder
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
-from langchain.chains.query_constructor.base import AttributeInfo
-from langchain_pinecone import PineconeVectorStore
-from langchain.retrievers.self_query.base import SelfQueryRetriever
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.runnables import RunnableLambda
+from langchain_community.document_transformers import LongContextReorder
+
+from operator import itemgetter
 
 # .env 파일에서 환경 변수 로드
 load_dotenv()
@@ -27,7 +31,7 @@ EMBEDDING_DIMENSION = 1024  # 모델에 맞게 차원 설정
 def load_model():
     model = ChatOpenAI(
         temperature=0.1,
-        model_name="gpt-4",  # GPT-4 모델 사용
+        model_name="gpt-4o-mini",
         streaming=True
     )
     print("model loaded...")
@@ -35,43 +39,45 @@ def load_model():
 
 def rag_chain(vectorstore):
     llm = load_model()
+
+    # 리트리버 설정
+    model = HuggingFaceCrossEncoder(model_name="BAAI/bge-reranker-v2-m3")
+    compressor_15 = CrossEncoderReranker(model=model, top_n=15)
+    vs_retriever30 = vectorstore.as_retriever(search_kwargs={"k": 30})
+    retriever = ContextualCompressionRetriever(base_compressor=compressor_15, base_retriever=vs_retriever30)
     
-    metadata_field_info = [
-        AttributeInfo(name='company',
-                      description='The card company written on the card_name',
-                      type='string'),
-        AttributeInfo(name='card_name',
-                      description='The name of the card held by the company',
-                      type='string'),
-        AttributeInfo(name='benefit',
-                      description='The name of the benefits you can receive when using the card listed on card_name',
-                      type='string')]
-
-    document_content_description = "Explanation of the benefits offered by a total of 10 cards"
-
-    self_query_retriever = SelfQueryRetriever.from_llm(
-        llm,
-        vectorstore,
-        document_content_description,
-        metadata_field_info,
+    # 리트리버 파이프라인
+    system_prompty = (
+        "Given a chat history and the latest user question "
+        "which might reference context in the chat history, "
+        "formulate a standalone question which can be understood "
+        "without the chat history. Please answer the question with new retrieved context, "
+        "just reformulate it if needed and otherwise return it as is."
     )
 
-    system_prompty = """When there is prior conversation history and a new user question, the question might be related to the previous conversation. 
-    Use the documents you retrieved in response to the previous question to answer the new question.
-    """
-    
     contextualize_prompt = ChatPromptTemplate.from_messages([
-        ("system",system_prompty),
+        ("system", system_prompty),
         MessagesPlaceholder("chat_history"),
-        ("human","{input}"),
+        ("human", "{input}"),
     ])
 
-    history_aware_retriever = create_history_aware_retriever(
+    history_aware_retriever_modified = create_history_aware_retriever(
         llm,
-        self_query_retriever,
+        retriever,
         contextualize_prompt
     )
 
+    # 문서 재정렬 추가
+    reordering = LongContextReorder()
+
+    my_retriever = (
+        {"input": itemgetter("input"),
+         "chat_history": itemgetter("chat_history")
+        } | history_aware_retriever_modified | 
+        RunnableLambda(lambda docs: reordering.transform_documents(docs))
+    )
+    
+    # LLM 체인 설정
     qa_system_prompt = """You are an assistant helping with question-answering tasks. 
     Use the retrieved information to answer the questions. 
     If the information includes details like card_name or specific benefits, make sure to include them in your answer. 
@@ -81,14 +87,15 @@ def rag_chain(vectorstore):
     {context}"""
 
     qa_prompt = ChatPromptTemplate.from_messages([
-        ("system",qa_system_prompt),
+        ("system", qa_system_prompt),
         MessagesPlaceholder("chat_history"),
-        ("human","{input}"),
+        ("human", "{input}"),
     ])
 
-    qa_chain = qa_prompt | llm | StrOutputParser()
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
 
-    return create_retrieval_chain(history_aware_retriever, qa_chain)
+    # RAG 체인 생성
+    return create_retrieval_chain(my_retriever, question_answer_chain)
 
 # 세션 기록을 저장할 딕셔너리
 store = {}
@@ -96,7 +103,6 @@ store = {}
 # 세션 ID를 기반으로 세션 기록을 가져오는 함수
 def get_session_history(session_ids):
     if session_ids not in store:  # 세션 ID가 store에 없는 경우
-        # 새로운 ChatMessageHistory 객체를 생성하여 store에 저장
         store[session_ids] = ChatMessageHistory()
     return store[session_ids]  # 해당 세션 ID에 대한 세션 기록 반환
 
